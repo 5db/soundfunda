683b42771b1680f863eb474696ff2c090024e243
think-mode-io <in.think.mode@gmail.com>
2013-07-22 20:20:54 -0400

    Fixes search bug

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ soundFunda.controller('SoundFundaController', function($scope, $rootScope) {
 
 	$scope.search = function(sound) {
 		SC.initialize({
-			client_id: 'CLIENT_ID_HERE',
+			client_id: 'b7b17489b877a6b30177725e0729c131',
 			redirect_uri: "http://localhost:9000/soundfunda/#/play"
 		});
 
 		SC.connect(function() {
-			SC.get("/tracks", {genres: $scope.sound.name, limit: 10}, function(tracks) {
+			SC.get("/tracks", {q: $scope.sound.name, limit: 20}, function(tracks) {
 				$rootScope.$apply(function() {
 					$scope.tracks = tracks;
 				});
@@ -37,7 +37,7 @@ soundFunda.controller('SoundFundaController', function($scope, $rootScope) {
 
 	$scope.play = function(track) {
 		SC.initialize({
-			client_id: 'CLIENT_ID_HERE',
+			client_id: 'b7b17489b877a6b30177725e0729c131',
 			redirect_uri: "http://localhost:9000/soundfunda/#/play"
 		});
 
